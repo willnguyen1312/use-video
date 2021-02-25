9560ee7b40cad8ffb9393282f911d7cd398a3c64
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-02-25 10:51:31 +0700

    Work it!

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,59 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef, useState, useEffect } from "react";
+
+import { useVideo } from "./useVideo";
+
+import "./App.css";
+
+const videoId = "videoId";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [videoOneElement, setVideoOneElement] = useState<HTMLVideoElement>();
+  const [videoTwoElement, setVideoTwoElement] = useState<HTMLVideoElement>();
+
+  const { state: stateOne, controls: controlsOne } = useVideo(videoOneElement);
+  const { state: stateTwo, controls: controlsTwo } = useVideo(videoTwoElement);
+
+  useEffect(() => {
+    const videoOne = videoRef.current;
+    if (videoOne) {
+      setVideoOneElement(videoOne);
+    }
+
+    const videoTwo = document.getElementById(videoId) as HTMLVideoElement;
+
+    if (videoTwo) {
+      setVideoTwoElement(videoTwo);
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <div className="App">
+        <video
+          width="50%"
+          ref={videoRef}
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+          controls
+        />
+        <video
+          width="50%"
+          id={videoId}
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+          controls
+        />
+      </div>
+
+      <div>
+        <button onClick={() => controlsOne.play()}>Play video one</button>
+        <h3>Video one current time: {stateOne.currentTime}</h3>
+      </div>
+      <div>
+        <button onClick={() => controlsTwo.play()}>Play video two</button>
+        <h3>Video two current time: {stateTwo.currentTime}</h3>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/useVideo.tsx
+++ src/useVideo.tsx
@@ -1,238 +1,185 @@
-import * as React from "react";
-import { useEffect, useRef } from "react";
-import useSetState from "./useSetState";
+import { useCallback, useState, useEffect } from "react";
 
-export interface HTMLMediaProps
-  extends React.AudioHTMLAttributes<any>,
-    React.VideoHTMLAttributes<any> {
-  src: string;
-}
+const useSetState = <T extends object>(
+  initialState: T = {} as T
+): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
+  const [state, set] = useState<T>(initialState);
+  const setState = useCallback(
+    (patch) => {
+      set((prevState) =>
+        Object.assign(
+          {},
+          prevState,
+          patch instanceof Function ? patch(prevState) : patch
+        )
+      );
+    },
+    [set]
+  );
+
+  return [state, setState];
+};
 
 export interface HTMLMediaState {
   buffered: TimeRanges | null;
   duration: number;
   paused: boolean;
   muted: boolean;
-  time: number;
+  currentTime: number;
   volume: number;
 }
 
-export interface HTMLMediaControls {
-  play: () => Promise<void> | void;
-  pause: () => void;
-  mute: () => void;
-  unmute: () => void;
-  volume: (volume: number) => void;
-  seek: (time: number) => void;
-}
+export const useVideo = (idOrEle?: string | HTMLMediaElement) => {
+  const [state, setState] = useSetState<HTMLMediaState>({
+    buffered: null,
+    currentTime: 0,
+    duration: 0,
+    paused: true,
+    muted: false,
+    volume: 1,
+  });
 
-type createHTMLMediaHookReturn = [
-  React.ReactElement<HTMLMediaProps>,
-  HTMLMediaState,
-  HTMLMediaControls,
-  { current: HTMLAudioElement | null }
-];
+  let mediaElement: HTMLMediaElement | undefined;
 
-export default function createHTMLMediaHook(tag: "audio" | "video") {
-  return (
-    elOrProps: HTMLMediaProps | React.ReactElement<HTMLMediaProps>
-  ): createHTMLMediaHookReturn => {
-    let element: React.ReactElement<any> | undefined;
-    let props: HTMLMediaProps;
-
-    if (React.isValidElement(elOrProps)) {
-      element = elOrProps;
-      props = element.props;
-    } else {
-      props = elOrProps as HTMLMediaProps;
-    }
-
-    const [state, setState] = useSetState<HTMLMediaState>({
-      buffered: null,
-      time: 0,
-      duration: 0,
-      paused: true,
-      muted: false,
-      volume: 1,
-    });
-    const ref = useRef<HTMLAudioElement | null>(null);
-
-    const wrapEvent = (userEvent, proxyEvent?) => {
-      return (event) => {
-        try {
-          proxyEvent && proxyEvent(event);
-        } finally {
-          userEvent && userEvent(event);
-        }
-      };
-    };
-
-    const onPlay = () => setState({ paused: false });
-    const onPause = () => setState({ paused: true });
-    const onVolumeChange = () => {
-      const el = ref.current;
-      if (!el) {
-        return;
+  const wrapEvent = (userEvent, proxyEvent?) => {
+    return (event) => {
+      try {
+        proxyEvent && proxyEvent(event);
+      } finally {
+        userEvent && userEvent(event);
       }
-      setState({
-        muted: el.muted,
-        volume: el.volume,
-      });
     };
-    const onDurationChange = () => {
-      const el = ref.current;
-      if (!el) {
-        return;
-      }
-      const { duration, buffered } = el;
-      setState({
-        duration,
-        buffered,
-      });
-    };
-    const onTimeUpdate = () => {
-      const el = ref.current;
-      if (!el) {
-        return;
-      }
-      setState({ time: el.currentTime });
-    };
-    const onProgress = () => {
-      const el = ref.current;
-      if (!el) {
-        return;
-      }
-      setState({ buffered: el.buffered });
-    };
-
-    if (element) {
-      element = React.cloneElement(element, {
-        controls: false,
-        ...props,
-        ref,
-        onPlay: wrapEvent(props.onPlay, onPlay),
-        onPause: wrapEvent(props.onPause, onPause),
-        onVolumeChange: wrapEvent(props.onVolumeChange, onVolumeChange),
-        onDurationChange: wrapEvent(props.onDurationChange, onDurationChange),
-        onTimeUpdate: wrapEvent(props.onTimeUpdate, onTimeUpdate),
-        onProgress: wrapEvent(props.onProgress, onProgress),
-      });
-    } else {
-      element = React.createElement(tag, {
-        controls: false,
-        ...props,
-        ref,
-        onPlay: wrapEvent(props.onPlay, onPlay),
-        onPause: wrapEvent(props.onPause, onPause),
-        onVolumeChange: wrapEvent(props.onVolumeChange, onVolumeChange),
-        onDurationChange: wrapEvent(props.onDurationChange, onDurationChange),
-        onTimeUpdate: wrapEvent(props.onTimeUpdate, onTimeUpdate),
-        onProgress: wrapEvent(props.onProgress, onProgress),
-      } as any); // TODO: fix this typing.
-    }
-
-    // Some browsers return `Promise` on `.play()` and may throw errors
-    // if one tries to execute another `.play()` or `.pause()` while that
-    // promise is resolving. So we prevent that with this lock.
-    // See: https://bugs.chromium.org/p/chromium/issues/detail?id=593273
-    let lockPlay: boolean = false;
-
-    const controls = {
-      play: () => {
-        const el = ref.current;
-        if (!el) {
-          return undefined;
-        }
-
-        if (!lockPlay) {
-          const promise = el.play();
-          const isPromise = typeof promise === "object";
-
-          if (isPromise) {
-            lockPlay = true;
-            const resetLock = () => {
-              lockPlay = false;
-            };
-            promise.then(resetLock, resetLock);
-          }
-
-          return promise;
-        }
-        return undefined;
-      },
-      pause: () => {
-        const el = ref.current;
-        if (el && !lockPlay) {
-          return el.pause();
-        }
-      },
-      seek: (time: number) => {
-        const el = ref.current;
-        if (!el || state.duration === undefined) {
-          return;
-        }
-        time = Math.min(state.duration, Math.max(0, time));
-        el.currentTime = time;
-      },
-      volume: (volume: number) => {
-        const el = ref.current;
-        if (!el) {
-          return;
-        }
-        volume = Math.min(1, Math.max(0, volume));
-        el.volume = volume;
-        setState({ volume });
-      },
-      mute: () => {
-        const el = ref.current;
-        if (!el) {
-          return;
-        }
-        el.muted = true;
-      },
-      unmute: () => {
-        const el = ref.current;
-        if (!el) {
-          return;
-        }
-        el.muted = false;
-      },
-    };
-
-    useEffect(() => {
-      const el = ref.current!;
-
-      if (!el) {
-        if (process.env.NODE_ENV !== "production") {
-          if (tag === "audio") {
-            console.error(
-              "useAudio() ref to <audio> element is empty at mount. " +
-                "It seem you have not rendered the audio element, which it " +
-                "returns as the first argument const [audio] = useAudio(...)."
-            );
-          } else if (tag === "video") {
-            console.error(
-              "useVideo() ref to <video> element is empty at mount. " +
-                "It seem you have not rendered the video element, which it " +
-                "returns as the first argument const [video] = useVideo(...)."
-            );
-          }
-        }
-        return;
-      }
-
-      setState({
-        volume: el.volume,
-        muted: el.muted,
-        paused: el.paused,
-      });
-
-      // Start media, if autoPlay requested.
-      if (props.autoPlay && el.paused) {
-        controls.play();
-      }
-    }, [props.src]);
-
-    return [element, state, controls, ref];
   };
-}
+
+  const onPlay = () => setState({ paused: false });
+  const onPause = () => setState({ paused: true });
+  const onVolumeChange = () => {
+    if (!mediaElement) {
+      return;
+    }
+    setState({
+      muted: mediaElement.muted,
+      volume: mediaElement.volume,
+    });
+  };
+  const onDurationChange = () => {
+    if (!mediaElement) {
+      return;
+    }
+    const { duration, buffered } = mediaElement;
+    setState({
+      duration,
+      buffered,
+    });
+  };
+  const onTimeUpdate = () => {
+    if (!mediaElement) {
+      return;
+    }
+    setState({ currentTime: mediaElement.currentTime });
+  };
+  const onProgress = () => {
+    if (!mediaElement) {
+      return;
+    }
+    setState({ buffered: mediaElement.buffered });
+  };
+
+  if (typeof idOrEle === "string") {
+    mediaElement = document.getElementById(idOrEle) as HTMLMediaElement;
+
+    if (!(mediaElement instanceof HTMLMediaElement)) {
+      throw new Error("Please pass in id of media element");
+    }
+  }
+
+  if (idOrEle instanceof HTMLMediaElement) {
+    mediaElement = idOrEle;
+  }
+
+  // Some browsers return `Promise` on `.play()` and may throw errors
+  // if one tries to execute another `.play()` or `.pause()` while that
+  // promise is resolving. So we prevent that with this lock.
+  // See: https://bugs.chromium.org/p/chromium/issues/detail?id=593273
+  let lockPlay: boolean = false;
+
+  const controls = {
+    play: () => {
+      if (!mediaElement) {
+        return undefined;
+      }
+
+      if (!lockPlay) {
+        const promise = mediaElement.play();
+        const isPromise = typeof promise === "object";
+
+        if (isPromise) {
+          lockPlay = true;
+          const resetLock = () => {
+            lockPlay = false;
+          };
+          promise.then(resetLock, resetLock);
+        }
+
+        return promise;
+      }
+      return undefined;
+    },
+    pause: () => {
+      if (mediaElement && !lockPlay) {
+        return mediaElement.pause();
+      }
+    },
+    seek: (time: number) => {
+      if (!mediaElement || !state.duration) {
+        return;
+      }
+      time = Math.min(state.duration, Math.max(0, time));
+      mediaElement.currentTime = time;
+    },
+    volume: (volume: number) => {
+      if (!mediaElement) {
+        return;
+      }
+      volume = Math.min(1, Math.max(0, volume));
+      mediaElement.volume = volume;
+      setState({ volume });
+    },
+    mute: () => {
+      if (!mediaElement) {
+        return;
+      }
+      mediaElement.muted = true;
+    },
+    unmute: () => {
+      if (!mediaElement) {
+        return;
+      }
+      mediaElement.muted = false;
+    },
+  };
+
+  useEffect(() => {
+    if (!mediaElement) {
+      return;
+    }
+
+    mediaElement.addEventListener("play", onPlay);
+    mediaElement.addEventListener("pause", onPause);
+    mediaElement.addEventListener("volumechange", onVolumeChange);
+    mediaElement.addEventListener("durationchange", onDurationChange);
+    mediaElement.addEventListener("timeupdate", onTimeUpdate);
+    mediaElement.addEventListener("progress", onProgress);
+
+    setState({
+      volume: mediaElement.volume,
+      muted: mediaElement.muted,
+      paused: mediaElement.paused,
+    });
+  }, [mediaElement, idOrEle]);
+
+  return {
+    state,
+    controls,
+  };
+};
